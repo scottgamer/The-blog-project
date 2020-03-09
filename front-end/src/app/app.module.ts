036a1eddbd9c9b575f8a69e306218cc8abe25304
richosojason@msn.com <richosojason@msn.com>
2020-03-09 11:40:38 -0500

    updated app.module

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { BlogListComponent } from './blog-list/blog-list.component';
+import { BlogItemComponent } from './blog-list/blog-item/blog-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BlogListComponent,
+    BlogItemComponent
   ],
   imports: [
     BrowserModule
