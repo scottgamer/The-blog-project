02bc61a6f6db668aedfe571b577436d84b53fa91
richosojason@msn.com <richosojason@msn.com>
2020-03-09 12:02:49 -0500

    added routing

---
+++ front-end/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from "@angular/core";
+
+import { Routes, RouterModule } from "@angular/router";
+import { BlogListComponent } from "./blog-list/blog-list.component";
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
+
+const appRoutes: Routes = [
+  { path: "", redirectTo: "/blogs", pathMatch: "full" },
+  { path: "blogs", component: BlogListComponent },
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,22 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { BlogListComponent } from './blog-list/blog-list.component';
-import { BlogItemComponent } from './blog-list/blog-item/blog-item.component';
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { BlogListComponent } from "./blog-list/blog-list.component";
+import { BlogItemComponent } from "./blog-list/blog-item/blog-item.component";
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BlogListComponent,
-    BlogItemComponent
+    BlogItemComponent,
+    LoginComponent,
+    RegisterComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
