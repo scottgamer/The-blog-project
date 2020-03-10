212c0f4fa4448432d4fcb5d207b517d0244103eb
richosojason@msn.com <richosojason@msn.com>
2020-03-10 11:59:43 -0500

    updated connection string

--- back-end/src/config/config.ts
+++ back-end/src/config/config.ts
@@ -1,6 +1,6 @@
-const { MONGODB_USER, MONGODB_PASSWORD } = process.env;
+const { MONGODB_USER, MONGODB_PASSWORD, MONGODB_DB } = process.env;
 
 export default {
-  mongoConnectionString: `mongodb://${MONGODB_USER}:${MONGODB_PASSWORD}@cluster0-shard-00-00-hwjmv.mongodb.net:27017,cluster0-shard-00-01-hwjmv.mongodb.net:27017,cluster0-shard-00-02-hwjmv.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`,
+  mongoConnectionString: `mongodb://${MONGODB_USER}:${MONGODB_PASSWORD}@cluster0-shard-00-00-hwjmv.mongodb.net:27017,cluster0-shard-00-01-hwjmv.mongodb.net:27017,cluster0-shard-00-02-hwjmv.mongodb.net:27017/${MONGODB_DB}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`,
   jwtSecret: process.env.JWT_SECRET
 };
