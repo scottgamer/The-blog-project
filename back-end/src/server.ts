203d917cdeee723d262d8a493b37208408c75793
richosojason@msn.com <richosojason@msn.com>
2020-03-10 14:58:28 -0500

    added mongo connection

---
+++ back-end/src/server.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import mongoose from "mongoose";
+import { applyMiddleware, applyRoutes } from "./utils";
+import middleware from "./middleware";
+import routes from "./routes";
+import config from "./config/config";
+
+mongoose
+  .connect(config.mongoConnectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    const app = express();
+    applyMiddleware(middleware, app);
+    applyRoutes(routes, app);
+
+    const { PORT = 3000 } = process.env;
+    app.listen(PORT, () => {
+      console.log(`Server is running on ${PORT}`);
+    });
+  })
+  .catch(error => console.log(error));
