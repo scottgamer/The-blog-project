2b2c7273c1fc4436fb50ef8bc9c549252a70c102
richosojason@msn.com <richosojason@msn.com>
2020-03-10 10:59:16 -0500

    added user model

---
+++ back-end/src/http/models/userModel.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Users", UserSchema);
