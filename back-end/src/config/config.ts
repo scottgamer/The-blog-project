3a8e236a9dcf22022fc45b4f172d4190e0ecf93e
richosojason@msn.com <richosojason@msn.com>
2020-03-11 10:52:20 -0500

    fixed config

--- back-end/src/config/config.ts
+++ back-end/src/config/config.ts
@@ -3,5 +3,5 @@ const { MONGODB_USER, MONGODB_PASSWORD, MONGODB_DB } = process.env;
 export default {
   mongoConnectionString: `mongodb://${MONGODB_USER}:${MONGODB_PASSWORD}@cluster0-shard-00-00-hwjmv.mongodb.net:27017,cluster0-shard-00-01-hwjmv.mongodb.net:27017,cluster0-shard-00-02-hwjmv.mongodb.net:27017/${MONGODB_DB}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`,
   jwtSecret: process.env.JWT_SECRET || "secret",
-  language: process.env.LANG || "es"
+  language: process.env.LANG || "en"
 };
