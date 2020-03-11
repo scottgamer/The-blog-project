87c717eb5357003b3ee6cd2f1bf89e66fc6076c5
richosojason@msn.com <richosojason@msn.com>
2020-03-11 17:23:13 -0500

    added onsubmit method in login.component

--- front-end/src/app/components/login/login.component.ts
+++ front-end/src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { NgForm } from "@angular/forms";
+import { AuthService } from "src/app/services/auth.service";
 
 @Component({
   selector: "app-login",
@@ -7,11 +9,35 @@ import { Router } from "@angular/router";
   styleUrls: ["./login.component.css"]
 })
 export class LoginComponent implements OnInit {
-  constructor(private router: Router) {}
+  isLoading = false;
+  error: string = null;
+
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {}
 
-  onLogin() {
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+
+    const value = form.value;
+    const { username, password } = value;
+
+    this.isLoading = true;
+    this.authService.logIn(username, password).subscribe(
+      response => {
+        this.isLoading = false;
+        this.navigateToProfile();
+      },
+      error => {
+        this.error = error.error.message[0].value;
+        this.isLoading = false;
+      }
+    );
+  }
+
+  private navigateToProfile() {
     this.router.navigate(["/profile"]);
   }
 }
