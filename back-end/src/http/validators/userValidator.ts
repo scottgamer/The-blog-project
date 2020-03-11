4c851c77b2df299acd9e31ba123076c9eb77d47b
richosojason@msn.com <richosojason@msn.com>
2020-03-11 10:10:19 -0500

    added async user validations

--- back-end/src/http/validators/userValidator.ts
+++ back-end/src/http/validators/userValidator.ts
@@ -1,20 +1,25 @@
 import { Request, Response, NextFunction } from "express";
 import Validator from "./validator";
+import "./customValidators";
 
 import transformErrors from "../transformers/errorTransformer";
 
 export const validateRegister = (req: Request, res: Response, next: NextFunction) => {
   const data = req.body;
   const rules = {
-    username: "required|string|min:4|max:10",
-    email: "required|email",
+    username: "required|string|min:4|max:10|username_available",
+    email: "required|email|email_available",
     password: "required|string|min:8|max:20"
   };
 
   const validation = new Validator(data, rules);
-  if (validation.fails()) {
-    res.status(200).send(transformErrors(validation.errors));
-  } else {
+  const passes = () => {
     next();
-  }
+  };
+
+  const fails = () => {
+    res.status(200).send(transformErrors(validation.errors));
+  };
+
+  validation.checkAsync(passes, fails);
 };
