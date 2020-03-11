2c48fc08b085f200491ce310d082324bdb7cb5cf
richosojason@msn.com <richosojason@msn.com>
2020-03-11 10:09:55 -0500

    added custom validator

---
+++ back-end/src/http/validators/customValidators.ts
@@ -0,0 +1,24 @@
+import Validator from "./validator";
+import * as userRepository from "../repositories/usersRepository";
+
+Validator.registerAsync(
+  "email_available",
+  async (email: string, args, req, passes) => {
+    if (await userRepository.findByEmail(email)) {
+      passes(false);
+    }
+    passes();
+  },
+  "The email is already registered"
+);
+
+Validator.registerAsync(
+  "username_available",
+  async (username: string, args, req, passes) => {
+    if (await userRepository.findByUsername(username)) {
+      passes(false);
+    }
+    passes();
+  },
+  "The username is already registered"
+);
