f11ee9aca0ac3093c7e18a418b80b80776bbf30e
richosojason@msn.com <richosojason@msn.com>
2020-03-11 11:50:38 -0500

    added onSUbmit method

--- front-end/src/app/components/register/register.component.ts
+++ front-end/src/app/components/register/register.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { AuthService } from "src/app/services/auth.service";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.css"]
 })
 export class RegisterComponent implements OnInit {
+  constructor(private authService: AuthService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    const value = form.value;
+    const { username, email, password } = value;
+
+    this.authService.signUp(username, email, password).subscribe(
+      response => console.log("response", response),
+      error => console.log("error", error)
+    );
+
+    form.reset();
   }
-
 }
