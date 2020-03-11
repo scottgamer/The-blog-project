645fbc31f6761be98db481daa8cf1611740b7778
richosojason@msn.com <richosojason@msn.com>
2020-03-11 11:50:50 -0500

    added auth.service

---
+++ front-end/src/app/services/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+import { environment } from "../../environments/environment";
+
+@Injectable({
+  providedIn: "root"
+})
+export class AuthService {
+  constructor(private httpClient: HttpClient) {}
+
+  signUp(username: string, email: string, password: string) {
+    return this.httpClient.post(`${environment.API_URL}/users`, {
+      username,
+      email,
+      password
+    });
+  }
+}
