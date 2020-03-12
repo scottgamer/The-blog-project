4fd0581c998124f5e3c957c93445597a984ae006
richosojason@msn.com <richosojason@msn.com>
2020-03-12 11:29:39 -0500

    added auth.guard to routes

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -7,12 +7,14 @@ import { RegisterComponent } from "./components/register/register.component";
 import { ProfileComponent } from "./components/profile/profile.component";
 import { AdminComponent } from "./components/admin/admin.component";
 
+import { AuthGuard } from "./guards/auth.guard";
+
 const appRoutes: Routes = [
   { path: "", redirectTo: "/blogs", pathMatch: "full" },
   { path: "blogs", component: BlogListComponent },
   { path: "login", component: LoginComponent },
   { path: "register", component: RegisterComponent },
-  { path: "profile", component: ProfileComponent },
+  { path: "profile", component: ProfileComponent, canActivate: [AuthGuard] },
   { path: "admin", component: AdminComponent }
 ];
 
