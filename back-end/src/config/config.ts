c488baaa2dd26e5d126fcbb1811cf649a0c20990
richosojason@msn.com <richosojason@msn.com>
2020-03-12 12:25:32 -0500

    updated default jwt secret

--- back-end/src/config/config.ts
+++ back-end/src/config/config.ts
@@ -2,6 +2,6 @@ const { MONGODB_USER = "scottgamer", MONGODB_PASSWORD = "laparosalping123", MONG
 
 export default {
   mongoConnectionString: `mongodb://${MONGODB_USER}:${MONGODB_PASSWORD}@cluster0-shard-00-00-hwjmv.mongodb.net:27017,cluster0-shard-00-01-hwjmv.mongodb.net:27017,cluster0-shard-00-02-hwjmv.mongodb.net:27017/${MONGODB_DB}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`,
-  jwtSecret: process.env.JWT_SECRET || "secret",
+  jwtSecret: process.env.JWT_SECRET || "secret123",
   language: process.env.LANG || "en"
 };
