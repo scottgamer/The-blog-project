5bc3b528fd8cf46af8d57a7564c5a732efdf48ad
richosojason@msn.com <richosojason@msn.com>
2020-03-12 11:29:17 -0500

    added auth guard to header.component

--- front-end/src/app/components/header/header.component.ts
+++ front-end/src/app/components/header/header.component.ts
@@ -1,16 +1,37 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { AuthService } from "src/app/services/auth.service";
+import { Router } from "@angular/router";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html",
   styleUrls: ["./header.component.css"]
 })
-export class HeaderComponent implements OnInit {
-  constructor() {}
+export class HeaderComponent implements OnInit, OnDestroy {
+  private userSubscription: Subscription;
+  isAuthenticated = false;
 
-  ngOnInit(): void {}
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    console.log("is authenticated", this.isAuthenticated);
+    this.userSubscription = this.authService.user.subscribe(user => {
+      this.isAuthenticated = !!user;
+      console.log("is authenticated", this.isAuthenticated);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
+  }
 
   logOut() {
-    //
+    this.authService.logOut();
+    this.navigateToHomePage();
+  }
+
+  private navigateToHomePage() {
+    this.router.navigate(["/"]);
   }
 }
