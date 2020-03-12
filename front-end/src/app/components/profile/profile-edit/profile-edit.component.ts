b579622c835794996e5dc0ba8f043a35e2b99e47
richosojason@msn.com <richosojason@msn.com>
2020-03-12 17:17:19 -0500

    wip profile edit component

--- front-end/src/app/components/profile/profile-edit/profile-edit.component.ts
+++ front-end/src/app/components/profile/profile-edit/profile-edit.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from "@angular/core";
-import { User } from "../../../models/user.model";
+import { Component, OnInit, ViewChild } from "@angular/core";
 import { UserService } from "src/app/services/user.service";
+import { LoggedUser } from "src/app/models/loggedUser.model";
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: "app-profile-edit",
@@ -8,10 +9,29 @@ import { UserService } from "src/app/services/user.service";
   styleUrls: ["./profile-edit.component.css"]
 })
 export class ProfileEditComponent implements OnInit {
-  user: User;
+  @ViewChild("editProfileForm", { static: false }) editProfileForm: NgForm;
+  loggedUser: LoggedUser;
+
   constructor(private userService: UserService) {}
 
   ngOnInit(): void {
-    this.userService.userSubject.subscribe(user => (this.user = user));
+    this.userService.loggedUserSubject.subscribe(loggedUser => {
+      this.loggedUser = loggedUser;
+
+      console.log("form", this.editProfileForm);
+      this.editProfileForm.setValue({
+        username: this.loggedUser.username,
+        email: this.loggedUser.email,
+        password: this.loggedUser.password
+      });
+    });
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+
+    const values = form.value;
   }
 }
