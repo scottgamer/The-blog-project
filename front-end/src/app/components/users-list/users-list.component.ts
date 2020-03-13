c2b7099c7b094be373ce2d39f369d80e10ab59f9
richosojason@msn.com <richosojason@msn.com>
2020-03-13 12:09:28 -0500

    added spinner to user-list.component

--- front-end/src/app/components/users-list/users-list.component.ts
+++ front-end/src/app/components/users-list/users-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { UserService } from "src/app/services/user.service";
+import { User } from "src/app/models/user.model";
 
 @Component({
   selector: "app-users-list",
@@ -6,7 +8,23 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./users-list.component.css"]
 })
 export class UsersListComponent implements OnInit {
-  constructor() {}
+  users: User[] = [];
+  isLoading = false;
+  constructor(private userService: UserService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.isLoading = true;
+
+    this.userService.getAllUsers().subscribe(
+      users => {
+        console.log(users);
+        this.users = users;
+        this.isLoading = false;
+      },
+      error => {
+        console.log(error);
+        this.isLoading = false;
+      }
+    );
+  }
 }
