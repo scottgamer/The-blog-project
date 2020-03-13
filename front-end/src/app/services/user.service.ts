6d6c15961edc280206f67cd79411989da2bb701a
richosojason@msn.com <richosojason@msn.com>
2020-03-13 12:10:29 -0500

    adde getall users method in user service

--- front-end/src/app/services/user.service.ts
+++ front-end/src/app/services/user.service.ts
@@ -28,7 +28,7 @@ export class UserService {
   }
 
   getAllUsers() {
-    return this.httpClient.get<LoggedUser>(`${environment.API_URL}/users`);
+    return this.httpClient.get<User[]>(`${environment.API_URL}/users`);
   }
 
   editUser(user: LoggedUser) {
