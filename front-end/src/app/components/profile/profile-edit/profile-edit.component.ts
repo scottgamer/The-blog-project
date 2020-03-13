9dae835d6f18d833a90151812e75dd9e20e86625
richosojason@msn.com <richosojason@msn.com>
2020-03-13 16:02:40 -0500

    added service to onsubmit method

--- front-end/src/app/components/profile/profile-edit/profile-edit.component.ts
+++ front-end/src/app/components/profile/profile-edit/profile-edit.component.ts
@@ -25,5 +25,8 @@ export class ProfileEditComponent implements OnInit {
     }
 
     const values = form.value;
+    this.userService
+      .updateUser(values)
+      .subscribe(response => console.log(response));
   }
 }
